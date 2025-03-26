badebf8a7fe3ae92d915deef67482aa660b98801
Hari-c137 <hariprasadaji465@gmail.com>
2025-03-26 22:15:12 +0530

    custom_color_theme_wallpaper

---
+++ color_scheme.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+last_img=$(ls ~/pics/* | sort -n | tail -1 |  awk -F'/' '{print $NF}' | awk -F'.' '{print $1}');
+next_img=$((last_img + 1));
+echo "enter the wallpaper link: ";  read link;
+echo "do you want to change color-scheme? (y/n)";
+read choice;
+
+if [[ $choice == "n" ]]
+then
+  curl -o $next_img $link;
+  mv $next_img ~/pics/;
+else
+  curl -O $link;
+  theme=$(gowall list | fzf);
+  filename=$(echo $link | awk -F'/' '{print $NF}');
+  gowall convert $filename -t $theme;
+  mv ~/Pictures/gowall/$filename $next_img;
+  mv $next_img ~/pics/;
+  rm $filename;
+fi
+  
