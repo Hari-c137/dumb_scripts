ea965bd4609de56d5ed60f418d357d7760f8704e
Hari-c137 <hariprasadaji465@gmail.com>
2025-04-14 22:36:16 +0530

    rand_wall script add

---
+++ rand_wall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+c_wbg=$(pgrep wbg)
+c_sbg=$(pgrep swaybg)
+PIC=($(find ~/pics/ -type f | shuf -n 2 --random-source=/dev/null))
+
+if xbps-query -Rs swaybg > /dev/null; then
+  if [[ c_wbg != NULL ]]; then
+    kill -9 $c_wbg
+  fi
+    wbg $PIC &
+else
+  if [[ c_sbg != NULL ]]; then
+    kill -9 $c_sbg
+  fi
+    swaybg -m fill -i $PIC &
+fi
+
+
