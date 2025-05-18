7c4b30613ddc1a1e66c8067ce9f3a0244f2cae34
Hari-c137 <hariprasadaji465@gmail.com>
2025-05-18 21:53:59 +0530

    added animations

--- swwwRand.sh
+++ swwwRand.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 WALLPAPER_DIR="${1:-$HOME/pics/}"
-PIC=($(find ~/pics/ -type f | shuf -n 2))
+effects=("grow" "wave" "any" "fade")
+random_index=$(( RANDOM % ${#effects[@]} )) 
 RANDOM_WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.webp" -o -iname "*.gif" -o  -iname "*.jpeg" -o -iname "*.bmp" \) | shuf -n 1)
-echo $RANDOM_WALLPAPER
-swww img $RANDOM_WALLPAPER &
+swww img -t ${effects[random_index]} $RANDOM_WALLPAPER &
