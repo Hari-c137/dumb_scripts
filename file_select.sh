9d3b84931241dabed72a33011c07a9001fabfab6
Hari-c137 <hariprasadaji465@gmail.com>
2025-05-19 18:07:28 +0530

    works!!

--- file_select.sh
+++ file_select.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Find non-hidden files and pass them to tofi for selection
-file=$(find . -type f -not -path "*/.*" |  tofi --anchor center --width 40% --height 40% --horizontal false --prompt-text "select file: ")
+# Find non-hidden files in ~/code/dumb_scripts and pass them to tofi for selection
+file=$(find ~/code/ -type f | tofi --anchor center --width 40% --height 40% --horizontal false --prompt-text "select file: ")
 
 if [[ -n "$file" ]]; then
-    neovide "$file"
+  fish -c " footclient nvim "$file""
 fi
 
