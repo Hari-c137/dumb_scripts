9100325a0248d6ce3df43b23f29a8e4e1b2b5d71
Hari-c137 <hariprasadaji465@gmail.com>
2025-05-25 17:49:12 +0530

    file_picker improvements

--- file_select.sh
+++ file_select.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# Find non-hidden files in ~/code/dumb_scripts and pass them to tofi for selection
-file=$(find ~/code/ -type f | tofi --anchor center --width 40% --height 40% --horizontal false --prompt-text "select file: ")
+env="$HOME/code"    # development enviroment diretory
 
+file=$(find $env -type f | tofi --anchor center --width 55% --height 40% --horizontal false --prompt-text "select file: ")
 if [[ -n "$file" ]]; then
-  fish -c " footclient nvim "$file""
+  cd $(dirname $file)
+  sh -c " kitty nvim "$file""
 fi
+
 
