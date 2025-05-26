b4331fa5d5da3616b0fd4ae5bf4d732262a7155f
Hari-c137 <hariprasadaji465@gmail.com>
2025-05-27 00:04:54 +0530

    added description

--- file_select.sh
+++ file_select.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-env="$HOME/code"    # development enviroment diretory
+# Author: Harry P.
+# Date:   2025-05-26 23:57
+# Header: file_select.sh
+# Desc:   file selector using tofi to launch file with neovim and kitty
 
-file=$(find $env -type f | tofi --anchor center --width 55% --height 40% --horizontal false --prompt-text "select file: ")
+env1="$HOME/code"    # development enviroment diretory
+env2="$HOME/.config"
+file=$(find $env1 $env2 -type f | tofi --anchor center --width 55% --height 40% --horizontal false --prompt-text "select file: ")
 if [[ -n "$file" ]]; then
   cd $(dirname $file)
   sh -c " kitty nvim "$file""
