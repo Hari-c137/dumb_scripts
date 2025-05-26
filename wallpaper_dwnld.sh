381fb8a83439f9b2f3c32365dc2525b81173070b
Hari-c137 <hariprasadaji465@gmail.com>
2025-05-26 23:50:12 +0530

    added description

--- wallpaper_dwnld.sh
+++ wallpaper_dwnld.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Author: Harry P.
+# Date:   2025-05-26 23:36
+# Header: wallpaper_dwnld.sh
+# Desc:   downloads walls & rename to numbers and store in pics
+
 last_img=$(ls ~/pics/* | sort -n | tail -1 |  awk -F'/' '{print $NF}' | awk -F'.' '{print $1}');
 next_img=$((last_img + 1));
 echo "enter the wallpaper link: ";  read link;
